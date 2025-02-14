fd08c6760a5065c3730f5a8c3dc9d7f2bc46d82e
Jéssica Máximo <122978824+jessicamaximo23@users.noreply.github.com>
2025-02-14 14:58:45 -0500

    add files

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,11 +3,12 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  cloud_firestore
-  firebase_auth
-  firebase_core
-  firebase_storage
+        cloud_firestore
+        firebase_auth
+        firebase_core
+        firebase_storage
 )
+
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
 )
