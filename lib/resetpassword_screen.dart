bc7900d122d07926165ef3a2fbe837461ee7db6d
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2025-02-19 15:40:58 -0500

    created a activity_main page to be me home screen

---
+++ lib/resetpassword_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class ResetPasswordScreen extends StatefulWidget {
+  @override
+  _ResetPasswordScreenState createState() => _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Reset Password"),
+      ),
+      body: Center(
+        child: Text("Reset Password Screen"),
+      ),
+    );
+  }
+}
