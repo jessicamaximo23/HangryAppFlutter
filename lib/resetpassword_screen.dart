ef222dafc46597890ce7e93fac6bdc0de0f710a3
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2025-02-20 19:34:48 -0500

    adding backend of password reset. need to implement on the structure yet.

--- lib/resetpassword_screen.dart
+++ lib/resetpassword_screen.dart
@@ -1,6 +1,43 @@
 import 'package:flutter/material.dart';
 
-class ResetPasswordScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ResetPasswordScreen extends StatefulWidget {
+  @override
+  _ResetPasswordScreenState createState() => _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void _resetPassword() async {
+    String email = _emailController.text.trim();
+
+    if (email.isEmpty) {
+      _showMessage("Please enter your email");
+      return;
+    }
+
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      _showMessage("Password reset email sent.");
+    } on FirebaseAuthException catch (e) {
+      String message = "An error occurred.";
+      if (e.code == 'user-not-found') {
+        message = "Email is not registered.";
+      } else {
+        message = e.message ?? message;
+      }
+      _showMessage(message);
+    }
+  }
+
+  void _showMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
