f28cef48af56442ce924651ee8565d053afb4bc5
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2025-02-20 20:00:56 -0500

    page UI updated for project standard

--- lib/resetpassword_screen.dart
+++ lib/resetpassword_screen.dart
@@ -49,13 +49,47 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
             children: [
               Image.asset('assets/logobackground.png', width: 200, height: 150),
               SizedBox(height: 20),
-              Text("Forgot Password"),
+              Text(
+                "Forgot Password",
+                style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue[800],
+              ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 15),
+              Text(
+                  "Enter your email address and we will send reset instructions.",
+                  style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+                  textAlign: TextAlign.center,
+                 ),
               SizedBox(height: 20),
-              Text("Enter your email address and we will send reset instructions."),
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: Icon(Icons.email, color: Colors.blue[800]),
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                ),
+              ),
               SizedBox(height: 20),
-              TextField(),
-              SizedBox(height: 20),
-              ElevatedButton(onPressed: () {}, child: Text("Reset Password")),
+              ElevatedButton(
+                    onPressed: _resetPassword,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber, // Cor personalizada
+                      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text("Reset Password",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+              ),
             ],
           ),
         ),
