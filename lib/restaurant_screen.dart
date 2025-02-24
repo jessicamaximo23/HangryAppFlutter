36a2eb452accb6866575b7e98b039d4708c33959
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2025-02-23 23:48:32 -0500

    added logout to Restaurant

--- lib/restaurant_screen.dart
+++ lib/restaurant_screen.dart
@@ -5,7 +5,25 @@ import 'package:firebase_auth/firebase_auth.dart';
 class RestaurantScreen extends StatefulWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
+  @override
+  _RestaurantScreen createState() => _RestaurantScreen();
 }
+
+class _RestaurantScreen extends State<RestaurantScreen> {
+
+  //SignOut created. Just add to the button you want to use.
+  void _signOut(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+
+      Navigator.pushReplacementNamed(context, '/login');
+    } catch (e) {
+      print("Error to log out: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to logout. Please try again.')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
