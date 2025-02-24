295f8abecfe9b4a2fab43391950b4e5981ef29be
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2025-02-23 23:51:53 -0500

    user07 sign out

--- lib/driver_screen.dart
+++ lib/driver_screen.dart
@@ -9,23 +9,6 @@ class DriverScreen extends StatefulWidget {
   _DriverScreen createState() => _DriverScreen();
 }
 
-class _DriverScreen extends State<DriverScreen> {
-
-  //SignOut created. Just add to the button you want to use.
-  void _signOut(BuildContext context) async {
-    try {
-      await FirebaseAuth.instance.signOut();
-
-      Navigator.pushReplacementNamed(context, '/login');
-    } catch (e) {
-      print("Error to log out: $e");
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to logout. Please try again.')),
-      );
-    }
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
