a299ec4125773e9fa5faa317c4a4bb3077808795
Wandrey C. Haagensen <142623622+WandreyCH@users.noreply.github.com>
2025-02-23 23:10:36 -0500

    sign out function created on user, driver and restaurant pages.

--- lib/user_screen.dart
+++ lib/user_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class UserScreen extends StatelessWidget {
+
+class UserScreen extends StatefulWidget {
   const UserScreen({Key? key}) : super(key: key);
+
+  @override
+  _UserScreenState createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+
+  //SignOut created. Just add to the button you want to use.
+  void _signOut(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+
+      Navigator.pushReplacementNamed(context, '/login');
+    } catch (e) {
+      print("Erro ao fazer logout: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Falha ao fazer logout. Tente novamente.')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
